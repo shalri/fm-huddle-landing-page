a55d5cdf64bfae35727b243420a81b13393741c0
shalri <fsaonlinework777@gmail.com>
2024-07-16 00:10:26 +0800

    Fixing feature code

--- components/Features.tsx
+++ components/Features.tsx
@@ -3,7 +3,7 @@ export default function Features() {
     <section className="">
       <div className="bg-hud-very-pale-blue pb-[52px] pt-[84px]">
         <div className="sm:mx-auto sm:flex sm:max-w-[1236px] sm:items-center sm:justify-evenly">
-          <div className="h-[210px] bg-[url('/fm-huddle-landing-page/images/illustration-grow-together.svg')] bg-contain bg-center bg-no-repeat sm:h-[425px] sm:w-[50%]" />
+          <div className="h-[210px] bg-[url(/fm-huddle-landing-page/images/illustration-grow-together.svg)] bg-contain bg-center bg-no-repeat sm:h-[425px] sm:w-[50%]" />
           <div className="order-first px-7 text-center text-hud-very-dark-cyan sm:w-[50%] sm:text-left">
             <h2 className="mt-[78px] font-poppins text-xl font-bold sm:mt-4 sm:text-[2.5rem]">
               Grow Together
@@ -16,15 +16,15 @@ export default function Features() {
           </div>
         </div>
       </div>
-      <div className="bg-white bg-[url(/fm-huddle-landing-page/images/bg-section-bottom-mobile-1.svg)] sm:bg-[url(/fm-huddle-landing-page/images/bg-section-bottom-desktop-1.svg)] bg-contain  bg-top bg-no-repeat pb-16 pt-[190px] sm:pt-[330px]">
+      <div className="bg-white bg-[url(/fm-huddle-landing-page/images/bg-section-bottom-mobile-1.svg)] sm:bg-[url(/images/bg-section-bottom-desktop-1.svg)] bg-contain  bg-top bg-no-repeat pb-16 pt-[190px] sm:pt-[330px]">
         <div className="sm:mx-auto sm:flex sm:max-w-[1256px] sm:items-center sm:justify-evenly">
-        <div className="h-[190px] bg-[url('/fm-huddle-landing-page/images/illustration-flowing-conversation.svg')] bg-contain bg-center bg-no-repeat sm:h-[375px] sm:w-[50%]" />
+        <div className="h-[190px] bg-[url(/fm-huddle-landing-page/images/illustration-flowing-conversation.svg)] bg-contain bg-center bg-no-repeat sm:h-[375px] sm:w-[50%]" />
         <div className="order-last sm: px-7 sm:pl-20 text-center text-hud-very-dark-cyan sm:w-[50%] sm:text-left">
           <h2 className="mt-[78px] font-poppins text-xl font-bold sm:mt-4 sm:text-[2.5rem]">
             Flowing Conversations
           </h2>
           <p className="mt-4 text-[14px] sm:mt-9 sm:max-w-[550px] sm:text-[16px]">
-            You wouldn't paginate a conversation in real life, so why do it
+            You wouldn&apos;t paginate a conversation in real life, so why do it
             online? Our threads have just-in-time loading for a more natural
             flow.
           </p>
@@ -34,7 +34,7 @@ export default function Features() {
       <div className="mt-[56px] sm:bg-[url(/fm-huddle-landing-page/images/bg-section-top-desktop-2.svg)] bg-[url(/fm-huddle-landing-page/images/bg-section-top-mobile-2.svg)] bg-contain bg-top bg-no-repeat pt-[103px]">
         <div className="sm:bg-hud-very-pale-blue sm:pt-[85px]">
         <div className="sm:pb-10 sm:mx-auto sm:flex sm:max-w-[1256px] sm:items-center sm:justify-evenly">
-          <div className="h-[200px] bg-hud-very-pale-blue bg-[url('/fm-huddle-landing-page/images/illustration-your-users.svg')] bg-contain bg-center bg-no-repeat sm:h-[400px] sm:w-[50%]" />
+          <div className="h-[200px] bg-hud-very-pale-blue bg-[url(/fm-huddle-landing-page/images/illustration-your-users.svg)] bg-contain bg-center bg-no-repeat sm:h-[400px] sm:w-[50%]" />
           <div className="order-first sm: px-7 sm:pl-7 text-center text-hud-very-dark-cyan sm:w-[50%] sm:text-left">
             <h2 className="mt-[78px] font-poppins text-xl font-bold sm:mt-4 sm:text-[2.5rem]">
               Your Users
